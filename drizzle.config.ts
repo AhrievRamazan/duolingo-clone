9d5c31b76f5c400556ff2a50cc8b84cff14e061a
AhrievRamazan <ramazanahriev278@gmail.com>
2025-05-12 02:48:56 +0300

    all mini change

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,8 +4,8 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./db/schema.ts",
   out: "./drizzle",
-  driver: "pg",
+  provider: "postgresql",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL!,
   },
-} satisfies Config;
+} as Config;
